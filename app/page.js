047985117d2c8e71cd5d4dff906263948e6739fc
Alex Bhusal <alexbhusal@icloud.com>
2025-03-19 00:50:17 +0545

    first commit

--- app/page.js
+++ app/page.js
@@ -1,103 +1,115 @@
-import Image from "next/image";
+"use client";
+import { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
+import CryptoJS from "crypto-js";
 
-export default function Home() {
+const Home = () => {
+  const [formData, setFormData] = useState({
+    amount: "",
+    tax_amount: "0",
+    total_amount: "10",
+    transaction_uuid: uuidv4(),
+    product_service_charge: "0",
+    product_delivery_charge: "0",
+    product_code: "EPAYTEST",
+    success_url: "http://localhost:3000/payment/success",
+    failure_url: "http://localhost:3000/payment/failure",
+    signed_field_names: "total_amount,transaction_uuid,product_code",
+    signature: "",
+    secret: "8gBm/:&EnhH.1/q",
+  });
+
+  const generateSignature = (
+    total_amount,
+    transaction_uuid,
+    product_code,
+    secret
+  ) =>
+    CryptoJS.enc.Base64.stringify(
+      CryptoJS.HmacSHA256(
+        `total_amount=${total_amount},transaction_uuid=${transaction_uuid},product_code=${product_code}`,
+        secret
+      )
+    );
+
+  useEffect(() => {
+    const { total_amount, transaction_uuid, product_code, secret } = formData;
+    setFormData((prevState) => ({
+      ...prevState,
+      signature: generateSignature(
+        total_amount,
+        transaction_uuid,
+        product_code,
+        secret
+      ),
+    }));
+  }, [formData.amount]);
+
+  const handleChange = ({ target }) =>
+    setFormData((prevState) => ({
+      ...prevState,
+      amount: target.value,
+      total_amount: target.value,
+    }));
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      <h1 className="text-center m-5 text-2xl font-semibold">NEXT ESEWA</h1>
+      <div className="flex flex-col md:flex-row gap-10 justify-center items-center">
+        <div className="border-black border-2 p-5 m-5 ">
+          <h1 className="text-center font-bold">For Testing Use </h1>
+          <h1>eSewa ID: <span className="text-rose-500 font-serif">9806800001</span></h1>
+          <h1>Password: <span className="text-indigo-400 font-serif">Nepal@123</span></h1>
+          <h1>MPIN: <span className="text-emerald-600 font-serif">1122</span></h1>
+          <h1>Token: <span className="text-orange-800 font-serif">123456</span></h1>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className="">
+          <form
+            action="https://rc-epay.esewa.com.np/api/epay/main/v2/form"
+            method="POST"
+          >
+            <input
+              type="text"
+              id="amount"
+              name="amount"
+              placeholder="Enter Amount"
+              className="border-2 border-gray-300 rounded-md p-2"
+              value={formData.amount}
+              onChange={handleChange}
+              required
+            />
+            {Object.entries(formData).map(
+              ([key, value]) =>
+                key !== "amount" &&
+                key !== "signature" && (
+                  <input
+                    key={key}
+                    type="hidden"
+                    name={key}
+                    value={value}
+                    required
+                  />
+                )
+            )}
+            <input
+              type="hidden"
+              id="signature"
+              name="signature"
+              value={formData.signature}
+              required
+            />
+              <div className="flex m-2 justify-center items-center">
+              <input
+                className="border-black border-4 p-2 bg-amber-100 font-semibold rounded-4xl"
+                value="Pay via E-Sewa"
+                type="submit"
+              />
+              </div>
+          </form>
+        </div>
+      </div>
+    </>
   );
-}
+};
+
+export default Home;
---
+++ app/payment/failure/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='h-screen flex items-center justify-center'> 
+      <h1 className='text-red-500 text-5xl'>Payment Failed</h1>
+    </div>
+  )
+}
+
+export default page
